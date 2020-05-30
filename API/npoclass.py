1cd8b4074e3f3bf7b5d16b31b754038093ed3d90
Ji Ma <magic.maji@gmail.com>
2020-05-30 21:33:23 +0000

    done API

--- API/npoclass.py
+++ API/npoclass.py
@@ -1,112 +1,119 @@
-################################### Define reproducibility ##########################
-# # Force using CPU.
-# # https://stackoverflow.com/questions/40690598/can-keras-with-tensorflow-backend-be-forced-to-use-cpu-or-gpu-at-will
-# import os
-# os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"   # see issue #152
-# os.environ["CUDA_VISIBLE_DEVICES"] = ""
-
-# obtain reproducible results
-
-import numpy as np
-import tensorflow as tf
-import random as rn
-
-# The below is necessary for starting Numpy generated random numbers
-# in a well-defined initial state.
-
-np.random.seed(42)
-
-# The below is necessary for starting core Python generated random numbers
-# in a well-defined state.
-
-rn.seed(12345)
-
-# Force TensorFlow to use single thread.
-# Multiple threads are a potential source of non-reproducible results.
-# For further details, see: https://stackoverflow.com/questions/42022950/
-
-session_conf = tf.ConfigProto(intra_op_parallelism_threads=1,
-                              inter_op_parallelism_threads=1)
-
-from keras import backend as K
-
-# The below tf.set_random_seed() will make random number generation
-# in the TensorFlow backend have a well-defined initial state.
-# For further details, see:
-# https://www.tensorflow.org/api_docs/python/tf/set_random_seed
-
-tf.set_random_seed(1234)
-
-sess = tf.Session(graph=tf.get_default_graph(), config=session_conf)
-K.set_session(sess)
-
-
-################################### Import dependencies ##########################
-from spellchecker import SpellChecker
-from keras.preprocessing.sequence import pad_sequences
-import pickle
-from keras.models import load_model
-import string
-import nltk
-nltk.download('stopwords')
-from nltk.corpus import stopwords
-stop_list=[str.upper(s) for s in stopwords.words('english')+list(string.punctuation)]
-import warnings
-warnings.simplefilter('ignore')
-import numpy as np
-from keras.utils import np_utils
-import os
+#set up environment
+import os, torch, pickle, warnings, random
 import pandas as pd
+import numpy as np
+from torch.utils.data import TensorDataset, DataLoader, RandomSampler, SequentialSampler
+from transformers import BertForSequenceClassification, BertTokenizer
+from tqdm import tqdm, trange
 import tensorflow as tf
-from multiprocessing import Pool
-p=Pool()
-
-
-################################### Load saved models and classes ##########################
-model_broad_cat=load_model('https://raw.githubusercontent.com/ma-ji/npo_classifier/master/output/broad_category_model.h5')
-model_major_group=load_model('https://raw.githubusercontent.com/ma-ji/npo_classifier/master/output/major_group_model.h5')
-with open('https://raw.githubusercontent.com/ma-ji/npo_classifier/master/output/tokenizer.pkl', 'rb') as tokenizer_pkl:
-    tokenizer = pickle.load(tokenizer_pkl)
-with open('https://raw.githubusercontent.com/ma-ji/npo_classifier/master/output/lb_broad_cat.pkl', 'rb') as lb_broad_cat_pkl:
-    lb_broad_cat = pickle.load(lb_broad_cat_pkl)
-with open('https://raw.githubusercontent.com/ma-ji/npo_classifier/master/output/lb_major_group.pkl', 'rb') as lb_major_group_pkl:
-    lb_major_group = pickle.load(lb_major_group_pkl)
-
-# String/String list input --> a list of string token list(s) --> spellchecking (parallel) --> predict class (serial).
-
+warnings.filterwarnings("ignore")
+from time import sleep
 
 ################################### Define functions ##########################
-def npoclass(string_input=None):
-    ## Define local function.
-    # Spell check function. Return corrected word if unknown; return original word if known.
-    def spellcheck(input_string):
-        if type(input_string)==str:
-            word_token_list=nltk.word_tokenize(input_string)
-            return [s.upper() for s in p.map(SpellChecker().correction, word_token_list)]
-        elif type(input_string)==list:
-            word_token_list_list=[nltk.word_tokenize(string) for string in input_string]
-            word_token_list_list_chk=[]
-            for word_token_list in word_token_list_list:
-                word_token_list_list_chk+=[[s.upper() for s in p.map(SpellChecker().correction, word_token_list)]]
-            return word_token_list_list_chk
-        else:
-            raise NameError('Input must be a string or a list of strings.')
+def npoclass(inputs, gpu_core=True, model_path='bert_model/'):
+   
+    # Set the seed value all over the place to make this reproducible.
+    seed_val = 42
+    random.seed(seed_val)
+    np.random.seed(seed_val)
+    torch.manual_seed(seed_val)
+
+    # Read model, if not read.
+    global model_loaded
+    try:
+        assert model_loaded
+    except:
+        #load a pretrained model and tokenizer.
+        model_loaded = BertForSequenceClassification.from_pretrained(model_path)
+        tokenizer_loaded = BertTokenizer.from_pretrained(model_path)
+        # Read label encoder.
+        with open(model_path+'le_broad_cat.pkl', 'rb') as label_encoder_pkl:
+            label_encoder = pickle.load(label_encoder_pkl)
     
-    result_dict={}
-    # Text to sequences.
-    # seq_encoding_text_test=tokenizer.texts_to_sequences(spellcheck(input_string_list))
-    seq_encoding_text=tokenizer.texts_to_sequences(spellcheck(string_input))
-    # Pads sequences to the same length (i.e., prepare matrix).
-    x_text=pad_sequences(sequences=seq_encoding_text,
-                        maxlen=46612, # Max length of the sequence.
-                        dtype = "int32", padding = "post", truncating = "post", 
-                        value = 0 # Zero is used for representing None or Unknown.
-                         )
-    # Predict.
-    y_prob=model_major_group.predict(x_text)
-    result_dict['major_group_label']=lb_major_group.inverse_transform(np_utils.to_categorical(y_prob.argmax(axis=-1))).tolist()
-    result_dict['major_group_prob']=[s.max() for s in y_prob]
-    y_prob=model_broad_cat.predict(x_text)
-    result_dict['broad_category_label']=lb_broad_cat.inverse_transform(np_utils.to_categorical(y_prob.argmax(axis=-1))).tolist()
-    result_dict['broad_category_prob']=[s.max() for s in y_prob]
-    return result_dict
+    # Select acceleration method.
+    if gpu_core==True and torch.cuda.is_available():
+        print('There are %d GPU(s) available.' % torch.cuda.device_count(), 'Using GPU:',torch.cuda.get_device_name(0))
+        torch.cuda.manual_seed_all(seed_val)
+        device = torch.device('cuda')
+        model_loaded.cuda()
+    else:
+        print('No GPU acceleration available or gpu_core=False, using CPU.')
+        device = torch.device('cpu')
+        model_loaded.cpu()
+    print('Encoding inputs ...')
+    sleep(.5) # Pause a second for better printing results.
+    # Tokenize all of the sentences and map the tokens to thier word IDs.
+    input_ids = []
+    attention_masks = []
+    # Encode inputs.
+    def func_encode_string(text_string):
+        encoded_dict = tokenizer_loaded.encode_plus(text_string,
+                                                    add_special_tokens = True, # Add '[CLS]' and '[SEP]'
+                                                    max_length = 256,           # Pad & truncate all sentences.
+                                                    pad_to_max_length = True,
+                                                    return_attention_mask = True,   # Construct attn. masks.
+                                                    return_tensors = 'pt',     # Return pytorch tensors.
+                                                   )
+        return encoded_dict
+    # Encode input string(s).
+    if type(inputs)==list:
+        for text_string in tqdm(inputs):
+            encoded_outputs=func_encode_string(text_string)
+            # Add the encoded sentence to the list.
+            input_ids.append(encoded_outputs['input_ids'])
+            # And its attention mask (simply differentiates padding from non-padding).
+            attention_masks.append(encoded_outputs['attention_mask'])
+    if type(inputs)==str:
+        encoded_outputs=func_encode_string(inputs)
+        input_ids=[encoded_outputs['input_ids']]
+        attention_masks=[encoded_outputs['attention_mask']]
+
+    # Convert the lists into tensors.
+    input_ids = torch.cat(input_ids, dim=0)
+    attention_masks = torch.cat(attention_masks, dim=0)
+
+    # Prepare dataloader for efficient calculation.
+    batch_size = 32
+    pred_data = TensorDataset(input_ids, attention_masks)
+    pred_sampler = SequentialSampler(pred_data)
+    pred_dataloader = DataLoader(pred_data, sampler=pred_sampler, batch_size=batch_size)
+
+    # Start prediction.
+    model_loaded.eval()
+    logits_all=[]
+    print('Predicting categories ...')
+    sleep(.5) # Pause a second for better printing results.
+    for batch in tqdm(pred_dataloader):
+        # Add batch to the pre-chosen device
+        batch = tuple(t.to(device) for t in batch)
+        b_input_ids, b_input_mask = batch
+        with torch.no_grad():
+            outputs = model_loaded(b_input_ids, token_type_ids=None, attention_mask=b_input_mask)
+        logits_all+=outputs[0].tolist()
+
+    # Calculate probabilities of logitcs.
+    logits_prob=tf.nn.sigmoid(logits_all).numpy().tolist()
+    # Find the positions of max values in logits.
+    logits_max=np.argmax(logits_prob, axis=1)
+    # Transfer to labels.
+    logits_labels=label_encoder.inverse_transform(logits_max)
+    
+    # Compile results to be returned.
+    result_list=[]
+    for list_index in range(0, len(logits_labels)):
+        result_dict={}
+        result_dict['recommended']=logits_labels[list_index]
+        conf_prob=logits_prob[list_index][logits_max[list_index]]
+        if conf_prob>=.99:
+            result_dict['confidence']='high (>=.99)'
+        elif conf_prob>=.95:
+            result_dict['confidence']='medium (<.99|>=.95)'
+        else:
+            result_dict['confidence']='low (<.95)'
+        prob_dict={}
+        for label_index in range(0, len(label_encoder.classes_)):
+            prob_dict[label_encoder.classes_[label_index]]=logits_prob[list_index][label_index]
+        result_dict['probabilities']=prob_dict
+        result_list+=[result_dict]
+        
+    return result_list
